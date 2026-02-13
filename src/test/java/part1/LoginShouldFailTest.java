d7833230d1322d22b6d49c09ad5e34e9eb817d8c
Molka Jlassi <jlassimolka80@gmail.com>
2026-02-13 02:16:01 +0100

    login

--- src/test/java/part1/LoginShouldFailTest.java
+++ src/test/java/part1/LoginShouldFailTest.java
@@ -9,7 +9,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class FirstSeleniumTest {
+public class LoginShouldFailTest {
     WebDriver driver;
     @BeforeClass
     public void  setUp(){
@@ -36,7 +36,7 @@ public class FirstSeleniumTest {
         Thread.sleep(2000);
         String actualResult= driver.findElement(By.tagName("h6")).getText();
         String expectedResult="Dashboard";
-        Assert.assertEquals(actualResult,expectedResult);
+        Assert.assertNotEquals(actualResult,expectedResult);
 
     }
 }
