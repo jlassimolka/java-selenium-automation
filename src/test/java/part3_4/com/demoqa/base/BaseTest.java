11049679c9c2f7d2cde9a941147cb609b92636ac
molkajlassi1 <jlassimolka80@gmail.com>
2026-06-30 23:48:09 +0100

    Update project

--- src/test/java/part3_4/com/demoqa/base/BaseTest.java
+++ src/test/java/part3_4/com/demoqa/base/BaseTest.java
@@ -3,9 +3,21 @@ package part3_4.com.demoqa.base;
 import com.demoqa.pages.HomePage;
 import com.saucedemo.pages.BasePage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
     private WebDriver driver;
     protected BasePage baspage;
     protected HomePage homePage;
+    private String DEMOQA_URL ="https://demoqa.com/";
+
+    @BeforeClass
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+    @BeforeMethod
+
 }
