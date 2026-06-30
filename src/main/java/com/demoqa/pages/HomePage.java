06cc2cef9c741cec3f96dc20f27d26e59524d899
molkajlassi1 <jlassimolka80@gmail.com>
2026-07-01 00:22:12 +0100

    Update project

--- src/main/java/com/demoqa/pages/HomePage.java
+++ src/main/java/com/demoqa/pages/HomePage.java
@@ -1,6 +1,12 @@
 package com.demoqa.pages;
 
 import com.saucedemo.pages.BasePage;
+import org.openqa.selenium.By;
 
 public class HomePage extends BasePage {
+    private By formsCard =  By.xpath("//div[@id='app']//h5[text()='Forms']");
+    public FormsPage goToForms(){
+     click(formsCard);
+     return newFormsPage();
+    }
 }
--- src/test/java/part3_4/com/demoqa/base/BaseTest.java
+++ src/test/java/part3_4/com/demoqa/base/BaseTest.java
@@ -4,12 +4,13 @@ import com.demoqa.pages.HomePage;
 import com.saucedemo.pages.BasePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
     private WebDriver driver;
-    protected BasePage baspage;
+    protected BasePage basePage;
     protected HomePage homePage;
     private String DEMOQA_URL ="https://demoqa.com/";
 
@@ -19,5 +20,15 @@ public class BaseTest {
         driver.manage().window().maximize();
     }
     @BeforeMethod
-
+    public void loadApplication()
+    {
+        driver.get(DEMOQA_URL);
+        basePage= new BasePage();
+        basePage.setDriver(driver);
+        homePage = new HomePage();
+    }
+ @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
 }
