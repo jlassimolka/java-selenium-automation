aece412e53046faaea6ab0302c71bc24e895cf0d
molkajlassi1 <jlassimolka80@gmail.com>
2026-06-30 23:34:32 +0100

    Update project

---
+++ src/test/java/part3_4/com/demoqa/base/BaseTest.java
@@ -0,0 +1,11 @@
+package part3_4.com.demoqa.base;
+
+import com.demoqa.pages.HomePage;
+import com.saucedemo.pages.BasePage;
+import org.openqa.selenium.WebDriver;
+
+public class BaseTest {
+    private WebDriver driver;
+    protected BasePage baspage;
+    protected HomePage homePage;
+}
