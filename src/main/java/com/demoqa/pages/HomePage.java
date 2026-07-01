a02cbad917318118905876cafcbee4959cdb7b08
molkajlassi1 <jlassimolka80@gmail.com>
2026-07-01 02:24:45 +0100

    Update project

--- src/main/java/com/demoqa/pages/HomePage.java
+++ src/main/java/com/demoqa/pages/HomePage.java
@@ -5,7 +5,7 @@ import com.saucedemo.pages.BasePage;
 import org.openqa.selenium.By;
 
 public class HomePage extends BasePage {
-    private By formsCard =  By.xpath("//div[@id='app']//h5[text()='Forms']");
+    private By formsCard =  By.xpath("//div[@id='root']//h5[text()='Forms']");
     public FormsPage goToForms(){
      click(formsCard);
      return new FormsPage();
