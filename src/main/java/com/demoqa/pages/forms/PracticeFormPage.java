ed30abc76473b1439dc673e96a74c3b71ede0e93
molkajlassi1 <jlassimolka80@gmail.com>
2026-07-03 21:22:58 +0100

    Update project

---
+++ src/main/java/com/demoqa/pages/forms/PracticeFormPage.java
@@ -0,0 +1,17 @@
+package com.demoqa.pages.forms;
+
+import org.openqa.selenium.By;
+
+import static utilities.JavaScriptUtility.clickJS;
+import static utilities.JavaScriptUtility.scrollToElementJS;
+
+public class PracticeFormPage extends FormsPage{
+    private By femaleRadioButton = By.id("gender-radio-2");
+
+    public void clickFemaleRadioButton() {
+        scrollToElementJS(femaleRadioButton);
+        clickJS(femaleRadioButton);
+    }
+
+
+}
